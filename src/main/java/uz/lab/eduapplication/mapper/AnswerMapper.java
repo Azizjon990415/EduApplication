95f3df8ce101fa7498b4b30378a484d47219142b
@Azizjon <azizjon990415@gmail.com>
2022-05-19 10:01:17 +0500

    service Example

--- src/main/java/uz/lab/eduapplication/mapper/AnswerMapper.java
+++ src/main/java/uz/lab/eduapplication/mapper/AnswerMapper.java
@@ -1,14 +1,32 @@
 package uz.lab.eduapplication.mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import uz.lab.eduapplication.DTO.AnswerDTO;
 import uz.lab.eduapplication.domain.Answer;
+import uz.lab.eduapplication.domain.Test;
+import uz.lab.eduapplication.repository.TestRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
 public class AnswerMapper {
+    @Autowired
+    TestRepository testRepository;
+    @Autowired
+    TestMapper testMapper;
+
     public AnswerDTO mapAnswerDomainToAnswerDTO(Answer answer){
-        return new AnswerDTO(answer.getId().toString(), answer.getIsCorrect(), answer.getText(), answer.getTest());
+        return new AnswerDTO(answer.getId().toString(), answer.getIsCorrect(), answer.getText(), testMapper.mapTestDomainToTestDTO(answer.getTest()));
     }
 
     public Answer mapAnswerDTOToAnswerDomain(AnswerDTO answerDTO){
-        return new Answer(answerDTO.getIsCorrect(), answerDTO.getText(), answerDTO.getTest());
+        Optional<Test> testOptional = testRepository.findById(UUID.fromString(answerDTO.getTestDTO().getId()));
+        if (testOptional.isPresent()){
+            return new Answer(answerDTO.getIsCorrect(), answerDTO.getText(), testOptional.get());
+        }else {
+            return new Answer(answerDTO.getIsCorrect(), answerDTO.getText(), testMapper.mapTestDTOToTestDomain(answerDTO.getTestDTO()));
+        }
     }
 }
---
+++ src/main/java/uz/lab/eduapplication/service/impl/SectionServiceImpl.java
@@ -0,0 +1,73 @@
+package uz.lab.eduapplication.service.impl;
+
+import lombok.AllArgsConstructor;
+import uz.lab.eduapplication.DTO.SectionDTO;
+import uz.lab.eduapplication.domain.Section;
+import uz.lab.eduapplication.mapper.SectionMapper;
+import uz.lab.eduapplication.repository.SectionRepository;
+import uz.lab.eduapplication.service.SectionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+@AllArgsConstructor
+public class SectionServiceImpl implements SectionService {
+    private SectionRepository sectionRepository;
+    private SectionMapper sectionMapper;
+    @Override
+    public List<SectionDTO> getAllSections() {
+        List<Section> sections = sectionRepository.findAll();
+        List<SectionDTO> sectionDTOS = new ArrayList<>();
+        sections.forEach(section -> {
+            SectionDTO sectionDTO = sectionMapper.mapSectionDomainToSectionDTO(section);
+            sectionDTOS.add(sectionDTO);
+        });
+        return sectionDTOS;
+    }
+
+    @Override
+    public SectionDTO getOneSection(UUID id) {
+        Optional<Section> optionalSection = sectionRepository.findById(id);
+        if (optionalSection.isPresent()){
+            Section section = optionalSection.get();
+            SectionDTO sectionDTO = sectionMapper.mapSectionDomainToSectionDTO(section);
+            return sectionDTO;
+        } else {
+            throw new NullPointerException("I can not find the Section with  id" + id);
+        }
+
+    }
+
+    @Override
+    public SectionDTO saveSection(SectionDTO sectionDTO) {
+        Section section = sectionMapper.mapSectionDTOToSectionDomain(sectionDTO);
+        Section savedSection = sectionRepository.save(section);
+        SectionDTO savedSectionDTO = sectionMapper.mapSectionDomainToSectionDTO(savedSection);
+        return savedSectionDTO;
+    }
+
+    @Override
+    public SectionDTO editSection(SectionDTO sectionDTO) {
+        boolean exists = sectionRepository.existsById(UUID.fromString(sectionDTO.getId()));
+        if (exists) {
+            Section section = sectionMapper.mapSectionDTOToSectionDomain(sectionDTO);
+            Section savedSection = sectionRepository.save(section);
+            SectionDTO savedSectionDTO = sectionMapper.mapSectionDomainToSectionDTO(savedSection);
+            return savedSectionDTO;
+        }else {
+            throw new NullPointerException("I can not find the Section with  id" + sectionDTO.getId());
+        }
+    }
+
+    @Override
+    public String deleteSection(UUID id) {
+        boolean exists = sectionRepository.existsById(id);
+        if (exists) {
+            sectionRepository.deleteById(id);
+            return "Data deleted";
+        }else {
+            throw new NullPointerException("I can not find the Section with  id" + id);
+        }
+    }
+}
