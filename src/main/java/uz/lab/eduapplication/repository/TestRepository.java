847ae15efd0d3e2e062222b4e6f8fdaba2eb9b4b
Sofia123234 <azizovashirina76@gmail.com>
2022-05-25 18:58:23 +0500

    gfffffd

--- src/main/java/uz/lab/eduapplication/repository/TestRepository.java
+++ src/main/java/uz/lab/eduapplication/repository/TestRepository.java
@@ -1,9 +1,14 @@
 package uz.lab.eduapplication.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import uz.lab.eduapplication.domain.Section;
 import uz.lab.eduapplication.domain.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TestRepository extends JpaRepository<Test, UUID> {
+    @Query(value = "select * from test where section = :sectionId",nativeQuery = true)
+    List<Test> findTestsByTestId(UUID testId);
 }
